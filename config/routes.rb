65e871fc5fc38dfece7ef409737e1614ec38bb45
Yongchol Choi <charleschoi87@gmail.com>
2017-05-23 03:15:26 +0000

    config/routes.rb 수정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
+  root 'posts#index'
+  resources :posts
+  resources :comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
