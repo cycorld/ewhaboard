9071428f2735dc26223a28af40974459002347fc
Yongchol Choi <charleschoi87@gmail.com>
2017-05-23 03:31:53 +0000

    posts#create 액션 만들기

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,14 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: :desc).all
   end
+  
+  def create
+    post = Post.new
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+    redirect_to root_path
+  end
 
   def show
   end
