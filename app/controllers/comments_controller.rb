02ab3183461872d17cc68934ed9a0e0ed874f8e1
Yongchol Choi <charleschoi87@gmail.com>
2017-05-23 03:49:23 +0000

    comments#create 액션 기능 구현

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+    def create
+        comment = Comment.new
+        comment.post_id = params[:post_id]
+        comment.content = params[:content]
+        comment.save
+        redirect_to root_path
+    end
 end
