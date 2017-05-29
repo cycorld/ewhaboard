aebbdd32190315976ea837f7577aed7d46a83ce1
Yongchol Choi <charleschoi87@gmail.com>
2017-05-29 23:12:43 +0000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529215425) do
+ActiveRecord::Schema.define(version: 20170529231215) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
