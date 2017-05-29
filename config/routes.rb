03ef9ef44fe2e8aaa9ba308746726e24c6278227
Yongchol Choi <charleschoi87@gmail.com>
2017-05-29 23:24:20 +0000

    3-b. 라우트 설정하기
    - Boards#index를 root로 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'boards/index'
-
-  get 'boards/show'
-
   mount RailsAdmin::Engine => '/boss', as: 'rails_admin'
   devise_for :users
   mount Ckeditor::Engine => '/ckeditor'
-  root 'posts#index'
+  root 'boards#index'
   resources :posts
   resources :comments
+  resources :boards
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
