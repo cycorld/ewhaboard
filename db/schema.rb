639c2c4cafa536831d3e59aa1761270092d5bc40
Yongchol Choi <charleschoi87@gmail.com>
2017-05-29 23:17:57 +0000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529231215) do
+ActiveRecord::Schema.define(version: 20170529231347) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170529231215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "board_id"
   end
 
   create_table "users", force: :cascade do |t|
