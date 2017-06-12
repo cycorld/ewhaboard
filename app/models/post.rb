d0160ca7c4d1ca43e2ece1ea716d60fb6918520f
Yongchol Choi <charleschoi87@gmail.com>
2017-06-12 21:41:04 +0000

    post 작성시 validation 체크하기

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :board_id, presence: true
     has_many :comments
     belongs_to :user
     belongs_to :board
