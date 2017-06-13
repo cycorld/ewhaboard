608f1961b97de01fc4f31718382eaca6fdfd5b54
Yongchol Choi <charleschoi87@gmail.com>
2017-06-13 05:13:37 +0000

    pins#create 액션 작성

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,4 +5,19 @@ class PinsController < ApplicationController
   def new
     @pin = Pin.new
   end
+  
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to pins_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def pin_params
+    params.require(:pin).permit(:title, :lat, :lng)
+  end
 end
