765ef4a445b2cfc9af4bd4a99bbae97072fc2f85
Yongchol Choi <charleschoi87@gmail.com>
2017-06-13 02:51:56 +0000

    위치 migration 파일 수정

--- db/migrate/20170613025032_create_pins.rb
+++ db/migrate/20170613025032_create_pins.rb
@@ -2,8 +2,8 @@ class CreatePins < ActiveRecord::Migration[5.0]
   def change
     create_table :pins do |t|
       t.string :title
-      t.decimal :lat
-      t.decimal :lng
+      t.decimal :lat, {:precision=>10, :scale=>6}
+      t.decimal :lng, {:precision=>10, :scale=>6}
 
       t.timestamps
     end
